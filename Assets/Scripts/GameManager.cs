6f689c0f6e05c15e32d840fbe27bf58537d72733
gmontsell <63999534+gmontsell@users.noreply.github.com>
2021-11-10 17:52:29 +0100

    health system gameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private List<Transform> chekPoints;
+    [SerializeField] private Text textGameOver;
+
+    private int last_chekPoint;
+
+    private void Start()
+    {
+        setLastCheckpoint(0);
+    }
+
+    public void gameOver()
+    {
+        textGameOver.gameObject.SetActive(true);
+        player.GetComponent<CharacterController>().enabled = false;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Restart();
+        }
+    }
+
+    public void Restart()
+    {
+        textGameOver.gameObject.SetActive(false);
+        player.GetComponent<HealthSystem>().restart();
+        player.GetComponent<CharacterController>().enabled = false;
+        player.transform.position = chekPoints[last_chekPoint].position;
+        player.transform.rotation = chekPoints[last_chekPoint].rotation;
+        player.GetComponent<CharacterController>().enabled = true;
+
+    }
+    public void setLastCheckpoint(int chekPoint)
+    {
+        chekPoints[last_chekPoint].gameObject.GetComponent<CheckPoint>().desactivateChekPoint();
+        last_chekPoint = chekPoint;
+        chekPoints[last_chekPoint].gameObject.GetComponent<CheckPoint>().activateChekPoint();
+    }
+}
---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void DieFunction();
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] float health = 100.0f;
+    [SerializeField] GameManager gameManager;
+    private float initialHealth = 0;
+    private DieFunction die;
+    
+    public void setDieFunction(DieFunction die)
+    {
+        this.die = die;
+    }
+    public void OnEnable()
+    {
+        initialHealth = health;
+    }
+    public void takeDamage(float value)
+    {
+        health -= value;
+        if (health <= 0.0f) Destroy(gameObject);
+    }
+
+    internal void restart()
+    {
+        
+        health = initialHealth;
+    }
+    public void kill()
+    {
+        health = 0;
+    }
+    private void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.M))
+        {
+            die();
+        }*/
+    }
+
+    internal void takeDamage(object v)
+    {
+        throw new NotImplementedException();
+    }
+}
